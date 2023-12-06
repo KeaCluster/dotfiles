7e90acb2891ea651b5178ab40abfff5d98eccf03
Zabdiel Diaz <37281456+KeaCluster@users.noreply.github.com>
2023-12-06 11:42:26 -0600

    Update nvim_setup.sh
    Added error handling

--- scripts/nvim_setup.sh
+++ scripts/nvim_setup.sh
@@ -4,17 +4,20 @@
 # Only for debian and WSL2 recommended
 
 echo "Downloading AppImage package"
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-
-# Download and setup AppImage
-if [ $? -eq 0 ]; then
+if curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage; then
     chmod u+x nvim.appimage
     echo "Extracting and setting up"
-    ./nvim.appimage --appimage-extract
-    sudo mv squashfs-root /opt/
-    sudo ln -s /opt/squashfs-root/AppRun /usr/bin/nvim
-    echo "Now you can run nvim by typing it in the terminal"
+    if ./nvim.appimage --appimage-extract; then
+        sudo mv squashfs-root /opt/
+        sudo ln -s /opt/squashfs-root/AppRun /usr/bin/nvim
+        echo "Neovim setup completed successfully"
+        # Optional: remove the downloaded appimage file
+        rm nvim.appimage
+    else
+        echo "Failed to extract AppImage"
+    fi
 else
     echo "Download failed"
 fi
+
 # Note: The latest stable inside apt is a non-compatible Ver with LazyVim
