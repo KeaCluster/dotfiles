e6803072743954e2ca9347d399ccba469c5789f2
Zabdiel Diaz <37281456+KeaCluster@users.noreply.github.com>
2023-12-06 11:43:08 -0600

    Update sys_links.sh
    Errors + handlers

--- scripts/sys_links.sh
+++ scripts/sys_links.sh
@@ -7,13 +7,21 @@
 DOTFILES_DIR="$HOME/dotfiles"
 TARGET_DIR="$HOME"
 
+# Function to create a symbolic link with backup
+create_link() {
+    if [ -f "$2" ] || [ -d "$2" ]; then
+        mv "$2" "$2.backup"
+        echo "Existing file/directory $2 moved to $2.backup"
+    fi
+    ln -s "$1" "$2"
+}
+
 # create symbolic links
-ln -s "$DOTFILES_DIR/bash/.profile" "$TARGET_DIR/.profile"
-ln -s "$DOTFILES_DIR/bash/.bashrc" "$TARGET_DIR/.bashrc"
-ln -s "$DOTFILES_DIR/git/.gitconfig" "$TARGET_DIR/.gitconfig"
-ln -s "$DOTFILES_DIR/nvim/nvim" "$TARGET_DIR/.config/nvim"
-ln -s "$DOTFILES_DIR/zsh/.zshrc" "$TARGET_DIR/.zshrc"
+create_link "$DOTFILES_DIR/bash/.profile" "$TARGET_DIR/.profile"
+create_link "$DOTFILES_DIR/bash/.bashrc" "$TARGET_DIR/.bashrc"
+create_link "$DOTFILES_DIR/git/.gitconfig" "$TARGET_DIR/.gitconfig"
+create_link "$DOTFILES_DIR/nvim" "$TARGET_DIR/.config/nvim"  # Ensure this path is correct
+create_link "$DOTFILES_DIR/zsh/.zshrc" "$TARGET_DIR/.zshrc"
 
 # Finished message
-
 echo "Symbolic links have been created"
