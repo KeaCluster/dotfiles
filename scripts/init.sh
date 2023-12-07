a4163f3447ef5dfa989295f1ce12f72647567742
Zabdiel Diaz <37281456+KeaCluster@users.noreply.github.com>
2023-12-07 15:20:21 -0600

    Update init.sh
    More error handling and optimization

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 # Update package list and install basic software
-sudo apt update
-sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 
-# Installs zsh, basic npm and openssh tools alongside c++ tools: build-essential && gdb
-sudo apt install -y zsh npm neofetch openssh-client curl build-essential gdb
+# Installs essential packages
+REQUIRED_PKGS="zsh nodejs npm openssh-client curl build-essential gdb neofetch"
+sudo apt install -y $REQUIRED_PKGS
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install Homebrew if not present
+if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 # Install Oh-My-Zsh if not already installed
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
@@ -34,14 +36,12 @@ create_link "$DOTFILES_DIR/bash/.bashrc" "$TARGET_DIR/.bashrc"
 create_link "$DOTFILES_DIR/git/.gitconfig" "$TARGET_DIR/.gitconfig"
 create_link "$DOTFILES_DIR/zsh/.zshrc" "$TARGET_DIR/.zshrc"
 
-# Install minimal theme for Zsh
-sh ./minimal_theme_setup.sh
+# Install minimal theme for Zsh (optional)
+[ -f ./minimal_theme_setup.sh ] && sh ./minimal_theme_setup.sh
 
-# Verify c++ tools
-echo "g++ location"
-whereis g++
-echo "gdb location"
-whereis gdb
+# Verify tools
+echo "g++ location: $(which g++)"
+echo "gdb location: $(which gdb)"
 
 # Messages
 echo "Installation and setup complete"
