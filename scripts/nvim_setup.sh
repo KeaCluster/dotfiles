90251b392bdeb98b11bba983d68b57ab9753f975
Zabdiel Diaz <37281456+KeaCluster@users.noreply.github.com>
2023-12-07 09:49:12 -0600

    Update nvim_setup.sh
    Added error handling and cleaned file

--- scripts/nvim_setup.sh
+++ scripts/nvim_setup.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-# Setup neovim config from Appimage
-# Only for debian and WSL2 recommended
+# Script to install Neovim from AppImage
 
-echo "Downloading AppImage package"
-if curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage; then
+# Download URL
+NVIM_APPIMAGE_URL="https://github.com/neovim/neovim/releases/latest/download/nvim.appimage"
+
+# Download Neovim AppImage
+echo "Downloading Neovim AppImage..."
+if curl -LO $NVIM_APPIMAGE_URL; then
     chmod u+x nvim.appimage
-    echo "Extracting and setting up"
-    if ./nvim.appimage --appimage-extract; then
-        sudo mv squashfs-root /opt/
-        sudo ln -s /opt/squashfs-root/AppRun /usr/bin/nvim
-        echo "Neovim setup completed successfully"
-        # Optional: remove the downloaded appimage file
-        rm nvim.appimage
-    else
-        echo "Failed to extract AppImage"
-    fi
+    echo "Neovim AppImage downloaded and made executable."
 else
-    echo "Download failed"
+    echo "Failed to download Neovim AppImage."
+    exit 1
 fi
+
+# Extract and install Neovim
+echo "Extracting and installing Neovim..."
+if ./nvim.appimage --appimage-extract; then
+    sudo mv squashfs-root /opt/nvim
+    sudo ln -s /opt/nvim/AppRun /usr/local/bin/nvim
+    echo "Neovim installed successfully."
+else
+    echo "Failed to extract and install Neovim."
+    exit 1
+fi
+
+# Cleanup
+rm nvim.appimage
+echo "Installation cleanup complete."
+
+echo "Neovim installation complete. Type 'nvim' to start."
+
 
 # Note: The latest stable inside apt is a non-compatible Ver with LazyVim
