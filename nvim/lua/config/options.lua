741e94c25ecf5da213943cbd34f93a3a2961d870
KeaCluster <zzabdiel29@gmail.com>
2023-12-15 10:13:20 -0600

    More custom opts

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,8 +1,31 @@
 local set = vim.opt
-set.tabstop = 8
+
+set.tabstop = 4
 set.softtabstop = 4
 set.shiftwidth = 4
 set.expandtab = true
 set.smartindent = true
 
 set.completeopt = "menu,menuone,noselect,noinsert"
+set.undodir = os.getenv("HOME") .. "/.vim/undodir"
+set.undofile = true
+set.signcolumn = "yes"
+
+
+-- cursor and movement
+
+set.guicursor = ""
+set.nu = true
+set.relativenumber = true
+set.scrolloff = 8
+
+set.smartindent = true
+set.wrap = false
+
+-- Seach utils
+
+set.hlsearch = false
+set.incsearch = true
+
+set.termguicolors = true
+set.colorcolumn = "80"
