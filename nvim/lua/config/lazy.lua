f3a68ece140a997fe478a36fbbef82066b5820fe
KeaCluster <zzabdiel29@gmail.com>
2023-12-19 14:01:39 -0600

    Cleaned plugin and added colorscheme again

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,10 +1,12 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
     -- bootstrap lazy.nvim
     -- stylua: ignore
     vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
         lazypath })
 end
+
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
@@ -47,6 +49,6 @@ require("lazy").setup({
     },
 })
 
--- added colorscheme as default
+-- kanagawa
 
 vim.cmd.colorscheme("kanagawa")
