311d254a4698ffd0303951731d643954c671b047
KeaCluster <zzabdiel29@gmail.com>
2023-12-18 23:29:19 -0600

    Added python3.11-venv package

--- scripts/init.sh
+++ scripts/init.sh
@@ -4,7 +4,7 @@
 sudo apt update && sudo apt upgrade -y
 
 # Installs essential packages
-REQUIRED_PKGS="zsh nodejs npm openssh-client curl build-essential gdb neofetch"
+REQUIRED_PKGS="zsh nodejs npm openssh-client curl build-essential gdb neofetch python3.11-venv"
 sudo apt install -y $REQUIRED_PKGS
 
 # Define Source and Target Path for symbolic links
@@ -13,11 +13,11 @@ TARGET_DIR="$HOME"
 
 # Function to create a symbolic link with backup
 create_link() {
-    if [ -f "$2" ] || [ -d "$2" ]; then
-        mv "$2" "$2.backup"
-        echo "Existing file/directory $2 moved to $2.backup"
-    fi
-    ln -s "$1" "$2"
+	if [ -f "$2" ] || [ -d "$2" ]; then
+		mv "$2" "$2.backup"
+		echo "Existing file/directory $2 moved to $2.backup"
+	fi
+	ln -s "$1" "$2"
 }
 
 # .zshrc requires a link before Oh-my-zsh is installed
@@ -25,7 +25,7 @@ create_link "$DOTFILES_DIR/zsh/.zshrc" "$TARGET_DIR/.zshrc"
 
 # Install Oh-My-Zsh if not already installed
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 # Create symbolic links
@@ -33,10 +33,9 @@ create_link "$DOTFILES_DIR/bash/.profile" "$TARGET_DIR/.profile"
 create_link "$DOTFILES_DIR/bash/.bashrc" "$TARGET_DIR/.bashrc"
 create_link "$DOTFILES_DIR/git/.gitconfig" "$TARGET_DIR/.gitconfig"
 
-
 # Install Homebrew if not present
-if ! command -v brew &> /dev/null; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v brew &>/dev/null; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Install minimal theme for Zsh (optional)
