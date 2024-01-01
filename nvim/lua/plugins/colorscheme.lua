65bcbc9fd44abce31482afd21b32a2d2f92a7f2e
KeaCluster <zzabdiel29@gmail.com>
2024-01-01 15:44:38 -0600

    Transparent kanagawa bg

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,4 +1,11 @@
 return {
-    "rebelot/kanagawa.nvim",
-    lazy = true,
+  "rebelot/kanagawa.nvim",
+  lazy = true,
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    }
+  }
 }
