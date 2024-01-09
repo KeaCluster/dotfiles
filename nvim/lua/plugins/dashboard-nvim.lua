a3d44f6e48fc7d8d884ddf971a280cad41587f9d
KeaCluster <zzabdiel29@gmail.com>
2024-01-08 21:32:54 -0600

    Fixed dashboard shortcuts not working +git repos

--- nvim/lua/plugins/dashboard-nvim.lua
+++ nvim/lua/plugins/dashboard-nvim.lua
@@ -1,6 +1,6 @@
 return {
-  'nvimdev/dashboard-nvim',
-  event = 'VimEnter',
+  "nvimdev/dashboard-nvim",
+  event = "VimEnter",
   config = function()
     local logo = [[
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣴⣶⡶⣶⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -21,34 +21,40 @@ return {
 
     logo = string.rep("\n", 8) .. logo .. "\n\n"
 
-    require('dashboard').setup {
-      theme = 'hyper',
+    require("dashboard").setup({
+      theme = "hyper",
       config = {
         header = vim.split(logo, "\n"),
         shortcut = {
-          { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+          { desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
           {
-            icon = ' ',
-            icon_hl = '@variable',
-            desc = 'Files',
-            group = 'Label',
-            action = 'Telescope find_files',
-            key = 'f',
+            icon = " ",
+            icon_hl = "@variable",
+            desc = "Files",
+            group = "Label",
+            action = "Telescope find_files",
+            key = "f",
           },
           {
-            desc = ' Apps',
-            group = 'DiagnosticHint',
-            action = 'Telescope app',
-            key = 'a',
+            desc = "󰊢 Repos",
+            group = "Find",
+            action = "Telescope git_files",
+            key = "g",
           },
           {
-            desc = ' dotfiles',
-            group = 'Number',
-            action = 'Telescope dotfiles',
-            key = 'd',
+            desc = " Apps",
+            group = "Find",
+            action = "Telescope find_files search_dirs=~/code",
+            key = "a",
           },
-        }
+          {
+            desc = " ~/.dotfiles",
+            group = "Number",
+            action = "Telescope find_files search_dirs=~/.dotfiles",
+            key = "d",
+          },
+        },
       },
-    }
+    })
   end,
 }
