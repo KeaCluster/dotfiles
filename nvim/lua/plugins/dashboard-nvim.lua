6406b939c14da1e944fc209289b1240714949df8
KeaCluster <zzabdiel29@gmail.com>
2024-01-16 20:13:52 -0600

    Honstly plenty of space

--- nvim/lua/plugins/dashboard-nvim.lua
+++ nvim/lua/plugins/dashboard-nvim.lua
@@ -28,7 +28,7 @@ return {
 ⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀⠀⠸⣿⣿⣿⣦⣀⣠⣴⡟⠋⢹⠀⠀⣀⣠⡴⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⢸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
     ]]
 
-    logo = string.rep("\n", 8) .. logo .. "\n\n"
+    logo = string.rep("\n", 4) .. logo .. "\n\n"
 
     require("dashboard").setup({
       theme = "hyper",
