7016cba0c2abf84a295ba61af8c7e24cd5570358
KeaCluster <zzabdiel29@gmail.com>
2024-01-23 12:02:59 -0600

    I dont even know if this works

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -8,4 +8,19 @@ return {
       floats = "transparent",
     },
   },
+  overrides = function(colors)
+    local theme = colors.theme
+    return {
+      normalfloat = { bg = "none" },
+      floatborder = { bg = "none" },
+      floattitle = { bg = "none" },
+      telescopetitle = { fg = theme.ui.special, bold = true },
+      telescopepromptnormal = { bg = theme.ui.bg_p1 },
+      telescopepromptborder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+      telescoperesultsnormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+      telescoperesultsborder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+      telescopepreviewnormal = { bg = theme.ui.bg_dim },
+      telescopepreviewborder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+    }
+  end,
 }
