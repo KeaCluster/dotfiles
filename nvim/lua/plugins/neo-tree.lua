217d6c56d0ec5a418836d0ea57f7b0a15f999b73
KeaCluster <zzabdiel29@gmail.com>
2024-02-23 15:48:22 -0600

    setup setting up

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -2,27 +2,25 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   config = function()
     require("neo-tree").setup({
-      opts = {
-        indent = {
-          with_markers = false,
+      indent = {
+        with_markers = false,
+      },
+      filesystem = {
+        filtered_items = {
+          visible = true,
         },
-        filesystem = {
-          filtered_items = {
-            visible = true,
-          },
-        },
-        window = {
-          width = 30, -- default is 40
-        },
-        git_status = {
-          symbols = {
-            modified = "",
-            untracked = "",
-            ignored = "",
-            unstaged = "󰄱",
-            staged = "",
-            conflict = "",
-          },
+      },
+      window = {
+        width = 30, -- default is 40
+      },
+      git_status = {
+        symbols = {
+          modified = "",
+          untracked = "",
+          ignored = "",
+          unstaged = "󰄱",
+          staged = "",
+          conflict = "",
         },
       },
     })
