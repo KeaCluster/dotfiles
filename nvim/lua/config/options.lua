8632cff409d9c1b28ec0c11c389a302809560bdf
KeaCluster <zzabdiel29@gmail.com>
2024-03-27 22:52:47 -0600

    Cleaning

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,5 +1,6 @@
 local set = vim.opt
 
+-- tab
 set.tabstop = 2
 set.softtabstop = 2
 set.shiftwidth = 2
@@ -9,10 +10,9 @@ set.smartindent = true
 -- this does nothign on terminal vim lmao
 set.linespace = 4
 
+-- comp menu
 vim.o.complete = ".,w,b,u,U,i,d,t"
 set.completeopt = "menu,menuone,noselect,noinsert"
-set.undofile = true
-set.signcolumn = "yes"
 
 -- cursor and movement
 set.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20"
@@ -22,6 +22,8 @@ set.nu = true
 set.relativenumber = true
 set.smartindent = true
 set.wrap = false
+set.scrolloff = 8
+set.sidescrolloff = 8
 
 -- characters
 set.list = true
@@ -45,9 +47,8 @@ set.colorcolumn = "80"
 set.showtabline = 0
 
 -- other
-set.scrolloff = 8
-set.sidescrolloff = 8
 set.confirm = true
+set.updatetime = 100
 
 -- somehow side bar with numbers isn't tarnsparent and some of this is necessary
 vim.cmd([[
