77fdb2f278b682ceba4a90480e0ad8fa37d98223
KeaCluster <zzabdiel29@gmail.com>
2024-07-05 09:14:56 -0600

    Plugin errors fix and tsserver errors

--- nvim/lua/plugins/mason-lock.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "zapling/mason-lock.nvim",
-  require("mason-lock").setup({
-    lockfile_path = vim.fn.stdpath("config") .. "/mason-lock.json",
-  }),
-}
--- nvim/lua/plugins/typescript-tools.lua
+++ nvim/lua/plugins/typescript-tools.lua
@@ -3,7 +3,7 @@ return {
   {
     "pmizio/typescript-tools.nvim",
     dependencies = {
-      "nvim-lua/plenary.nvim", -- typescript-tools.nvim depends on plenary.nvim
+      "nvim-lua/plenary.nvim","neovim/nvim-lspconfig" -- typescript-tools.nvim depends on plenary.nvim
     },
     config = function()
       require("typescript-tools").setup({})
