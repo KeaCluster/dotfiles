5a6c957b6b7cfae9cfd390f920ce09917c43dcfb
KeaCluster <zzabdiel29@gmail.com>
2024-09-04 09:34:22 -0600

    Neo-tree switched keymaps

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -2,6 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   config = function()
     require("neo-tree").setup({
+      close_if_last_window = true,
       indent = {
         with_markers = false,
       },
@@ -22,6 +23,24 @@ return {
           staged = "",
           conflict = "",
         },
+      },
+      keys = {
+        {
+          "<leader>fe",
+          function()
+            require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+          end,
+          desc = "Explorer NeoTree (cwd)",
+        },
+        {
+          "<leader>fE",
+          function()
+            require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+          end,
+          desc = "Explorer NeoTree (Root Dir)",
+        },
+        { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (cwd)", remap = true },
+        { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (Root Dir)", remap = true },
       },
     })
   end,
