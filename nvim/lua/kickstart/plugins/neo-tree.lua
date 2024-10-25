2bdc259fe7ddb3f83b7fc683b30fe2be5c7f3d01
KeaCluster <zzabdiel29@gmail.com>
2024-10-25 09:23:25 -0600

    neo-tree upd8

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -8,13 +8,32 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
+  event = 'VimEnter',
   cmd = 'Neotree',
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
+    close_if_last_window = true,
     filesystem = {
       window = {
+        width = 30,
+        git_status = {
+          symbols = {
+            modified = '',
+            untracked = '',
+            ignored = '',
+            unstaged = '󰄱',
+            staged = '',
+            conflict = '',
+          },
+        },
+        indent = {
+          with_markers = false,
+        },
+        filtered_items = {
+          visible = true,
+        },
         mappings = {
           ['\\'] = 'close_window',
         },
